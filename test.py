6a1c79017eb2f45516dd3fdcb695a65fccb500e8
colin <pollockcolin@gmail.com>
2011-03-15 18:52:12 -0700

    Adding empty test file.

---
+++ test.py
@@ -0,0 +1,10 @@
+
+from crosswordnik import make_puzzle
+
+class TestCrossword(object):
+    def setup(self):
+        puzzle = make_puzzle(10, 10, 10) 
+
+    test_words(self):
+        """Test whether all spans of letters are in fact words."""
+        pass
